694579ea7db6652e021e5069281622fa5f24ba90
andrenius <andrenius@gmail.com>
2020-01-27 20:55:00 +0700

    Решение через webserver (flask)

---
+++ src/interface/webserver/app.py
@@ -0,0 +1,40 @@
+"""
+Приложение интерфейса веб сервера.
+"""
+
+import os
+from flask import Flask, render_template, request
+
+from src.interface.core.commands.manager import run_help_cmd, run_cmd, UnknownCmd
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+app.config.from_object(__name__)
+
+
+@app.route('/', methods=['post', 'get'])
+def _index():
+    command_line = request.form.get('cl')
+    params = str(command_line or '').split()
+
+    result_list = []
+    if len(params) < 1:
+        result_list = run_help_cmd()
+    else:
+        try:
+            result_list = (run_cmd(params[0], params[1:]))
+        except UnknownCmd as err:
+            result_list = [f'Ошибка! {str(err)}'] + run_help_cmd()
+
+    return render_template('index.html', **locals())
+
+
+def run():
+    """
+    Запустить сервер.
+    """
+
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    run()
